4d7159f0ddfcaa15cf7cc58bcc3281ae3c6c5c61
JohanNordenadler <johan.nordenadler@student.hv.se>
2023-04-24 21:43:50 +0200

    connection

--- FREIIA-API/Program.cs
+++ FREIIA-API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<FREIIAContext>(options =>
-    options.UseSqlite(@"Data Source=FREIIA.db"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

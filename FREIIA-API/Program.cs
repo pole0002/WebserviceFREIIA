3c6bb31d296aad7a534fe61bb0be9601c10fea20
Polina Levnell <polina.parole.levnell@gmail.com>
2023-04-26 09:33:49 +0200

    policy + changed version of sql and tools

--- FREIIA-API/Program.cs
+++ FREIIA-API/Program.cs
@@ -4,14 +4,18 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<FREIIAContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// establishing Policy requirements: 
+builder.Services.AddCors(options => options.AddPolicy("Policy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()));
+
 builder.Services.AddSwaggerGen();
 
 
@@ -25,7 +29,8 @@ app.UseSwaggerUI();
 //}
 
 app.UseAuthorization();
+app.UseCors("MyPolicy");
 
-app.MapControllers();
+app.MapControllers().RequireCors("Policy");
 
 app.Run();

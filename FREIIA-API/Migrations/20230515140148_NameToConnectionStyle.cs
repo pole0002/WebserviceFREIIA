e37a3f8dd2b6bd9ff7f95a5019e195ae4394df16
Kenny Gustavsson <xtromenia@gmail.com>
2023-05-15 16:02:13 +0200

    ChartIdToColors

---
+++ FREIIA-API/Migrations/20230515140148_NameToConnectionStyle.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FREIIA_API.Migrations
+{
+    public partial class NameToConnectionStyle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ConnectionStyles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ChartId",
+                table: "Colors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ConnectionStyles");
+
+            migrationBuilder.DropColumn(
+                name: "ChartId",
+                table: "Colors");
+        }
+    }
+}

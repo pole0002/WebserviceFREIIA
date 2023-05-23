20b4cb7571319c6181d243a1ce7b1e1241411f8e
Polina Levnell <polina.parole.levnell@gmail.com>
2023-05-23 10:01:31 +0200

    save changes - not done yet
    -

--- FREIIA-API/Controllers/RolesController.cs
+++ FREIIA-API/Controllers/RolesController.cs
@@ -111,10 +111,10 @@ namespace FREIIA_API.Controllers
             }
             else if (role != null)
             {
-                // Getting a pist of participants who has this role ID
+                // Getting a list of participants who has this role ID
                 List<Participant> participants = _context.Participants.Where(p => p.RoleId == id).ToList();
 
-                // We will update all participants from the list from the chosen roleID to -1 (cannot set to null due to that it is an integer, not a string)
+                // We will update all participants from the list from the chosen roleID to null (cannot set to null due to that it is an integer, not a string)
                 foreach (Participant participant in participants)
                 {
                     participant.RoleId = null;
